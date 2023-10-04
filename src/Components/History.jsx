eae50d61956a2e5d18d6f21f3e46c589304e3a24
Bhumi <bhumione9545@gmail.com>
2023-10-04 18:44:29 +0530

    Adding final fixes

--- src/Components/History.jsx
+++ src/Components/History.jsx
@@ -8,13 +8,13 @@ const History = () => {
   const navigate = useNavigate();
   const wordsList = useSelector((state) => state.addToHistoryReducer);
   return (
-    <div className='w-screen h-screen bg-stone-950 flex flex-col items-center justify-start gap-4 pt-10'>
-      <ul className='max-w-96 flex items-start justify-start gap-5'>
+    <div className='w-screen h-screen bg-stone-950 flex flex-col items-center justify-start gap-4 pt-10 px-10'>
+      <ul className='max-w-96 flex flex-wrap items-center justify-center text-center gap-5'>
         {wordsList.length > 0 &&
           wordsList.map((word) => (
             <li
               onClick={() => navigate(`/history/word/${word?.word}`)}
-              className='w-full text-white font-bold text-xl py-2 px-4 bg-green-400 rounded-full cursor-pointer'
+              className=' md:w-1/4 w-full text-white font-bold text-xl py-2 px-4 bg-green-400 rounded-full cursor-pointer'
               key={word?.id}
             >
               {word?.word}
